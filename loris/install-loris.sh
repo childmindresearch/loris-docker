1c82d5404df0766c0d98f0538361c39998d2b1b2
Gabriel Schubiner <gabriel.schubiner@childmind.org>
2024-07-12 15:50:19 -0400

    Add support for installing and configuring instruments

--- loris/install-loris.sh
+++ loris/install-loris.sh
@@ -3,13 +3,12 @@
 set -e
 
 # Set directory environment variables.
-BASE_PATH="/var/www/loris/"
+BASE_PATH="/var/www/loris"
 DATA_DIR="/data/${PROJECT_NAME}/data/"
 
-_load_secrets() {
-    MYSQL_PASSWORD=$(<${MYSQL_PASSWORD_FILE})
-    LORIS_ADMIN_PASSWORD=$(<${LORIS_ADMIN_PASSWORD_FILE})
-}
+# Load Secrets
+MYSQL_PASSWORD=$(<${MYSQL_PASSWORD_FILE})
+LORIS_ADMIN_PASSWORD=$(<${LORIS_ADMIN_PASSWORD_FILE})
 
 _update_config() {
     mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} \
@@ -29,39 +28,63 @@ _update_admin_site() {
         -e "UPDATE user_psc_rel SET CenterID=(SELECT ID FROM psc WHERE Name='${SITE_NAME}') WHERE ID=1" ${MYSQL_DATABASE}
 }
 
-#INSERT INTO psc (Name, Alias, MRI_alias, Study_site) VALUES ('${SITE_NAME}','${SITE_ALIAS}','${MRI_ALIAS}','${STUDY_SITE_YN}');
-_initialize_study_db() {
+# UPDATE psc SET Name='${SITE_NAME}', Alias='${SITE_ALIAS}', MRI_alias='${MRI_ALIAS}', Study_site='${STUDY_SITE_YN}' WHERE CenterID=1;
+# Site will have CenterID=2
+_initialize_site() {
     mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} <<EOF
-UPDATE psc SET Name='${SITE_NAME}', Alias='${SITE_ALIAS}', MRI_alias='${MRI_ALIAS}', Study_site='${STUDY_SITE_YN}' WHERE CenterID=1;
-INSERT INTO Visit_Windows (Visit_label,  WindowMinDays, WindowMaxDays, OptimumMinDays, OptimumMaxDays, WindowMidpointDays) VALUES ('${VISIT_LABEL}', ${WINDOW_MIN_DAYS}, ${WINDOW_MAX_DAYS}, ${OPTIMUM_MIN_DAYS}, ${OPTIMUM_MAX_DAYS}, ${WINDOW_MIDPOINT_DAYS});
-INSERT INTO visit (VisitName, VisitLabel) VALUES ('${VISIT_NAME}', '${VISIT_LABEL}');
+INSERT INTO psc (Name, Alias, MRI_alias, Study_site) VALUES ('${SITE_NAME}','${SITE_ALIAS}','${MRI_ALIAS}','${STUDY_SITE_YN}');
 EOF
 }
 
+_initialize_visits() {
+    for V in ${VISIT_LABELS}; do
+        mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} <<EOF
+INSERT INTO Visit_Windows (Visit_label,  WindowMinDays, WindowMaxDays, OptimumMinDays, OptimumMaxDays, WindowMidpointDays) VALUES ('${V}', ${WINDOW_MIN_DAYS}, ${WINDOW_MAX_DAYS}, ${OPTIMUM_MIN_DAYS}, ${OPTIMUM_MAX_DAYS}, ${WINDOW_MIDPOINT_DAYS});
+INSERT INTO visit (VisitName, VisitLabel) VALUES ('${V}', '${V}');
+EOF
+    done
+}
+
 _install_instruments() {
-    cd /var/www/loris/tools/
+    cd "${BASE_PATH}/tools/"
     for instrument_file in /etc/loris_instruments/*.linst; do
         local instrument_filename=$(basename ${instrument_file})
         local instrument=${instrument_filename%.linst}
-        if [[ -f "/var/www/loris/project/instruments/${instrument_filename}" ]]; then
+        if [[ -f "${BASE_PATH}/project/instruments/${instrument_filename}" ]]; then
             echo "Skipping instrument ${instrument_file} as it already exists."
             continue
         fi
         echo "Installing instrument ${instrument}..."
-        cp ${instrument_file} /var/www/loris/project/instruments/
+        cp ${instrument_file} "${BASE_PATH}/project/instruments/"
         echo "Generating ${instrument} SQL tables and testNames..."
-        php generate_tables_sql_and_testNames.php < ${instrument_file}
+        php generate_tables_sql_and_testNames.php <${instrument_file}
         echo "Installing ${instrument} in database..."
-        mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /var/www/loris/project/tables_sql/${instrument}.sql
+        mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} <"${BASE_PATH}/project/tables_sql/${instrument}.sql"
         echo "Done installing ${instrument}."
-        chown -R lorisadmin:www-data /var/www/loris/project/instruments/ /var/www/loris/project/tables_sql/
+        chown -R lorisadmin:www-data "${BASE_PATH}/project/instruments/" "${BASE_PATH}/project/tables_sql/"
     done
     cd -
 }
 
-install_loris() {
-    _load_secrets
+_install_instrument_battery() {
+    echo "Setting up Loris battery with all instruments..."
+    mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} <<EOF
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, Visit_label, CenterID) 
+    SELECT Test_name, ${DEFAULT_TEST_AGE_MIN_DAYS}, ${DEFAULT_TEST_AGE_MAX_DAYS}, 'Y', '${DEFAULT_TEST_STAGE}', 'VisitLabel', 2 FROM test_names CROSS JOIN visit;
+EOF
+}
+
+_install_db_schema() {
+    echo "Setting up Loris database schema..."
+    for sql_file in "${BASE_PATH}/SQL/0000*.sql"; do
+        echo "Installing ${sql_file}..."
+        mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} <${sql_file}
+    done
+}
+
+_install_loris() {
     echo "Loris configuration does not exist. Installing configuration with values:"
+    echo "PROJECT_NAME: ${PROJECT_NAME}"
     echo "MYSQL_HOST: ${MYSQL_HOST}"
     echo "MYSQL_USER: ${MYSQL_USER}"
     echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
@@ -82,7 +105,7 @@ install_loris() {
     _update_config "JWTKey" $(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9!\$^@#%&*()' | fold -w 32 | head -n 1)
 
     # Copy base configuration.
-    cp /var/www/loris/docs/config/config.xml /var/www/loris/project/config.xml
+    cp "${BASE_PATH}/docs/config/config.xml" "${BASE_PATH}/project/config.xml"
 
     # Replace placeholders with environment variables.
     sed -i \
@@ -90,25 +113,44 @@ install_loris() {
         -e "s/%USERNAME%/${MYSQL_USER}/g" \
         -e "s/%PASSWORD%/${MYSQL_PASSWORD}/g" \
         -e "s/%DATABASE%/${MYSQL_DATABASE}/g" \
-        /var/www/loris/project/config.xml
-    chown lorisadmin:www-data /var/www/loris/project/config.xml
-    chmod 660 /var/www/loris/project/config.xml
-
-    _initialize_study_db
+        "${BASE_PATH}/project/config.xml"
+    chown lorisadmin:www-data "${BASE_PATH}/project/config.xml"
+    chmod 660 "${BASE_PATH}/project/config.xml"
 }
 
-# Install Loris configuration if DEBUG_CONTAINER is not set.
-echo "Configuring Loris..."
-if [ ! -f /var/www/loris/projects/config.xml ] && [[ -z "${DEBUG_CONTAINER}" ]]; then
+# Install Loris configuration.
+if [ ! -f "${BASE_PATH}/projects/config.xml" ] && [[ -z "${DEBUG_CONTAINER}" ]]; then
     echo "Loris configuration does not exist. Installing configuration."
-    install_loris
+    _install_db_schema
+    _install_loris
+
+    if [[ -n "${SITE_NAME}" ]]; then
+        echo "Setting up Loris site..."
+        _initialize_site
+    else
+        echo "Skipping site initialization."
+    fi
+
+    if [[ -n "${VISIT_LABELS}" ]]; then
+        echo "Setting up Loris visits..."
+        _initialize_visits
+    else
+        echo "VISIT_LABELS is not set. Skipping visit initialization."
+    fi
+
+    if [[ -d /etc/loris_instruments ]]; then
+        echo "Installing instruments..."
+        _install_instruments
+    else
+        echo "Instruments directory does not exist. Skipping instrument installation."
+    fi
+
+    if [[ -n "${INSTALL_INSTRUMENT_BATTERY}" ]]; then
+        echo "Setting up Loris database with user additions..."
+        _install_instrument_battery
+    else
+        echo "Skipping battery installation."
+    fi
 else
     echo "Skipping Loris configuration installation."
-fi
-
-if [[ -d /etc/loris_instruments ]]; then
-    echo "Installing instruments..."
-    _install_instruments
-else
-    echo "Instruments directory does not exist. Skipping instrument installation."
 fi
